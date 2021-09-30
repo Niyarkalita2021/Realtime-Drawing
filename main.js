24be332821ad61a92e7d2afb18811b2a1ee00ede
Niyarkalita2021 <82107686+Niyarkalita2021@users.noreply.github.com>
2021-09-30 20:30:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
   video = createCapture(VIDEO);
   video.size(550, 500);
@@ -18,9 +24,22 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX +  " difference = " + difference);
     }
 }
 function draw() {
     background('#FFFF8F');
     
+    document.getElementById("square_side").innerHTML = "width and height of a square will be = " + difference + "px";
+    fill('#00FFFF');
+    stroke('#00688B');
+    square(noseX, noseY, difference);
 }
